f1ec9145d927662985584645f27fd8eb889ba6ee
Darkhan Akmyrza <darkhanakmyrza1@gmail.com>
2020-07-22 18:11:07 +0500

    added req.txt

--- courses/fields.py
+++ courses/fields.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import redirect, get_object_or_404
-from django.views.generic.base import TemplateResponseMixin, View
-from .forms import ModuleFormSet
+
 
 class OrderField(models.PositiveIntegerField):
     def __init__(self, for_fields=None, *args, **kwargs):
@@ -30,24 +29,3 @@ class OrderField(models.PositiveIntegerField):
         else:
             return super(OrderField, self).pre_save(model_instance, add)
 
-class CourseModuleUpdateView(TemplateResponseMixin, View):
-template_name = 'courses/manage/module/formset.html'
-course = None
-def get_formset(self, data=None):
-return ModuleFormSet(instance=self.course,
-data=data)
-def dispatch(self, request, pk):
-self.course = get_object_or_404(Course,
-id=pk,
-owner=request.user)
-return super(CourseModuleUpdateView, self).dispatch(request, pk)
-def get(self, request, *args, **kwargs):
-formset = self.get_formset()
-return self.render_to_response({'course': self.course, 'formset': formset})
-def post(self, request, *args, **kwargs):
-formset = self.get_formset(data=request.POST)
-if formset.is_valid():
-formset.save()
-return redirect('manage_course_list')
-return self.render_to_response({'course': self.course,
-'formset': formset})
